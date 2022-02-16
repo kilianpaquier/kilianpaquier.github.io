77e7f755866f737afdd1f23b86b36ea810719f9c
Kilian PAQUIER <contact@kilian-paquier.fr>
2022-02-16 10:07:20 +0100

    misc|lint fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -98,7 +98,7 @@ registerLocaleData(localeEn, "en");
             provide: RECAPTCHA_LANGUAGE,
             useFactory: (locale: string) => locale,
             deps: [LOCALE_ID],
-        }
+        },
     ],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
