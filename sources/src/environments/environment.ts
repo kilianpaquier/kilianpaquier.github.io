1520bd994d55d1cbbcb1ee8479945ca028020738
Kilian PAQUIER <contact@kilian-paquier.fr>
2022-03-16 19:49:07 +0100

    misc|fix title + added current version to footer

--- sources/src/environments/environment.ts
+++ sources/src/environments/environment.ts
@@ -5,6 +5,7 @@ export const environment = {
     captchaSiteKey: process.env.CAPTCHA_SITE_KEY || "",
     formspreeFrUrl: process.env.FORMSPREE_FR_URL || "",
     formspreeGitlabUrl: process.env.FORMSPREE_GITLAB_URL || "",
+    version: process.env.IMAGE_VERSION || "",
     httpOptions: {
         headers: new HttpHeaders({
             "Content-Type": "application/json",
