5e4ad7e083eb8457cc9ef1947467cc1cd32bf97f
Kilian PAQUIER <contact@kilian-paquier.fr>
2022-07-14 20:38:08 +0200

    main| adapt PWA configuration

--- sources/vue.config.js
+++ sources/vue.config.js
@@ -7,5 +7,17 @@ module.exports = defineConfig({
         plugins: [
             new VuetifyLoaderPlugin()
         ]
+    },
+    pwa: {
+        themeColor: "white",
+        name: "Kilian PAQUIER",
+        iconPaths: {
+            faviconSVG: "favicon.ico",
+            favicon32: "favicon.ico",
+            favicon16: "favicon.ico",
+            appleTouchIcon: "favicon.ico",
+            maskIcon: "favicon.ico",
+            msTileImage: "favicon.ico"
+        }
     }
 });
